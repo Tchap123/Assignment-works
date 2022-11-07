3eb489f502ad0595ed1b5add362d873c9c7a56ad
Tchap123 <102277221+Tchap123@users.noreply.github.com>
2022-11-07 22:17:08 +0530

    Update as3q1.java
    The program has been updated

--- as3q1.java
+++ as3q1.java
@@ -7,7 +7,8 @@ class summ{
 		System.out.print("Enter the sum of number you want:");
 		int n=in.nextInt();
 		int summ=0;
-		for (int i=0;i<=n;i++)	summ+=i;
-		System.out.println("sum: "+summ);
-	}
-}
+		for (int i=0;i<=n;i++){
+			summ+=i;
+			System.out.println("sum: "+summ);
+		}
+}}
