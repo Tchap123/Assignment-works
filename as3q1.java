15c5626711a1b8f73cb2cd5f192fa22a6ed1c09a
Tchap123 <102277221+Tchap123@users.noreply.github.com>
2022-11-07 04:19:47 -0500

    Add files via upload
    Assignments given at Object Oriented Programming Lab class by the professor at my college;

---
+++ as3q1.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class summ{
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		System.out.print("Enter the sum of number you want:");
+		int n=in.nextInt();
+		int summ=0;
+		for (int i=0;i<=n;i++)	summ+=i;
+		System.out.println("sum: "+summ);
+	}
+}
